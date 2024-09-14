389d009b389abdc0e7a3edde61c1c534ff1ff761
Jonathan Parker <jonathanp62@gmail.com>
2024-09-14 14:11:55 -0400

    Upgraded the version to 0.3.0

--- src/main/java/net/jmp/demo/forkjoinpool/Version.java
+++ src/main/java/net/jmp/demo/forkjoinpool/Version.java
@@ -1,11 +1,12 @@
 package net.jmp.demo.forkjoinpool;
 
 /*
+ * (#)Version.java  0.3.0   09/14/2024
  * (#)Version.java  0.2.0   09/14/2024
  * (#)Version.java  0.1.0   09/14/2024
  *
  * @author    Jonathan Parker
- * @version   0.2.0
+ * @version   0.3.0
  * @since     0.1.0
  *
  * MIT License
@@ -36,7 +37,7 @@ package net.jmp.demo.forkjoinpool;
  */
 final class Version {
     /** The version. */
-    static final String VERSION_STRING = "0.2.0";
+    static final String VERSION_STRING = "0.3.0";
 
     /**
      * The default constructor.
